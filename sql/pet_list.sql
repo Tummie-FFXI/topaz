f6066884267ef10c3a1af18d50a4fea64b45e3e7
Zach Toogood <zrtoogood@gmail.com>
2020-02-29 15:01:42 +0200

    GEO SQL updates

--- sql/pet_list.sql
+++ sql/pet_list.sql
@@ -116,7 +116,4 @@ INSERT INTO `pet_list` VALUES (71, 'SharpshotFrame', 5126, 1, 99, 0, 0);
 INSERT INTO `pet_list` VALUES (72, 'StormwakerFrame', 5127, 1, 99, 0, 0);
 -- INSERT INTO `pet_list` VALUES (73, 'AdventuringFellow', 0, 1, 99, 0, 0);
 -- 74 is Chocobo in the enum..
-
-
--- trusts.. TODO: Make trusts not use petlist at all.
-INSERT INTO `pet_list` VALUES (75, 'Shantotto', 5896, 1, 99, 0, 0);
+INSERT INTO `pet_list` VALUES (75, 'Luopan', 6020, 1, 99, 0, 0);
