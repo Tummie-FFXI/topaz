54b0fc78aee4eeed32c0ec42593d909467299d4d
Zach Toogood <zrtoogood@gmail.com>
2020-03-13 23:25:10 +0200

    Add timer and tag

--- scripts/globals/abilities/cover.lua
+++ scripts/globals/abilities/cover.lua
@@ -10,21 +10,22 @@ require("scripts/globals/utils")
 require("scripts/globals/msg")
 -----------------------------------
 
-function onAbilityCheck(player,target,ability)
-    if (target:getID() == player:getID() or not target:isPC()) then
-        return dsp.msg.basic.CANNOT_ON_THAT_TARG
+function onAbilityCheck(player, target, ability)
+    if target:getID() == player:getID() or not target:isPC() then
+        return tpz.msg.basic.CANNOT_ON_THAT_TARG
     else
         return 0
     end
 end
 
-function onUseAbility(player,target,ability)
-    --formula from https://www.bg-wiki.com/bg/Cover
+function onUseAbility(player, target, ability)
     local baseDuration = 15
-    local bonusTime = utils.clamp(math.floor((player:getMod(dsp.mod.VIT) + player:getMod(dsp.mod.MND) - target:getMod(dsp.mod.VIT) * 2) / 4), 0, 15)
+    local bonusTime = utils.clamp(math.floor((player:getMod(tpz.mod.VIT) + player:getMod(tpz.mod.MND) - target:getMod(tpz.mod.VIT) * 2) / 4), 0, 15)
     local duration = baseDuration + bonusTime
 
-    player:addStatusEffect(dsp.effect.COVER,0,0,duration)
-    player.addMod(dsp.mod.Enmity, 200)
-    target:lowerEnmity(target, 10)
+    player:addStatusEffect(tpz.effect.COVER, 0, 0, duration)
+    target:setLocalVar("COVER", player:getID())
+    target:timer(duration * 1000, function(target)
+        target:setLocalVar("COVER", 0)
+    end)
 end
