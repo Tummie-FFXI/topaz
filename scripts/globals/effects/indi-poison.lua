3bfeba9c7b1bacb3d056aed5cc58b5c221ee2b09
Zach Toogood <zrtoogood@gmail.com>
2020-03-28 10:31:08 +0200

    Effects for Poison II and Regen II

--- scripts/globals/effects/indi-poison.lua
+++ scripts/globals/effects/indi-poison.lua
@@ -9,7 +9,7 @@ function onEffectGain(target, effect)
 end
 
 function onEffectTick(target, effect)
-    tpz.geo.applyEffectOverArea(target, tpz.effect.POISON_II, tpz.allegiance.MOB)
+    tpz.geo.applyEffectOverArea(target, tpz.effect.POISON, tpz.allegiance.MOB)
 end
 
 function onEffectLose(target, effect)
